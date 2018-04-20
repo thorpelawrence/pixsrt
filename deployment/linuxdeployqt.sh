2b0f7e934a6645ff1d2e554add4d9189537a29d0
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2018-04-20 23:45:22 +0100

    Cleanup macdeployqt travis [skip appveyor]

--- deployment/linuxdeployqt.sh
+++ deployment/linuxdeployqt.sh
@@ -7,4 +7,4 @@ if [ -z "$desktopintegration" ] ; then ./linuxdeployqt*.AppImage ./appdir/usr/sh
 if [ ! -z "$desktopintegration" ] ; then . appimage-desktopintegration.sh ; fi
 find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 mv ./Pixsrt*.AppImage ./pixsrt-${TRAVIS_TAG}-linux-64${desktopintegration}.AppImage
-curl --upload-file ./pixsrt-${TRAVIS_TAG}-linux-64${desktopintegration}.AppImage https://transfer.sh/pixsrt-${TRAVIS_TAG}-${CC}-64${desktopintegration}.AppImage
+curl --upload-file ./pixsrt-${TRAVIS_TAG}-linux-64${desktopintegration}.AppImage https://transfer.sh/pixsrt-${TRAVIS_TAG}-${CXX}-64${desktopintegration}.AppImage
