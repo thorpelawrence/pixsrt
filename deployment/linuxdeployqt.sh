0e4a80633dc1605e041bbdf7229f3b1440b35ef2
Lawrence Thorpe <thorpe_lawrence@yahoo.co.uk>
2018-04-22 20:57:48 +0100

    Fix linux desktopintegration [skip appveyor]

--- deployment/linuxdeployqt.sh
+++ deployment/linuxdeployqt.sh
@@ -4,7 +4,7 @@ chmod a+x linuxdeployqt*.AppImage
 unset QTDIR; unset QT_PLUGIN_PATH ; unset LD_LIBRARY_PATH
 ./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -bundle-non-qt-libs
 if [ -z "$desktopintegration" ] ; then ./linuxdeployqt*.AppImage ./appdir/usr/share/applications/*.desktop -appimage ; fi
-if [ ! -z "$desktopintegration" ] ; then . appimage-desktopintegration.sh ; fi
+if [ ! -z "$desktopintegration" ] ; then . deployment/appimage-desktopintegration.sh ; fi
 find ./appdir -executable -type f -exec ldd {} \; | grep " => /usr" | cut -d " " -f 2-3 | sort | uniq
 mv ./Pixsrt*.AppImage ./pixsrt-${TRAVIS_TAG}-linux-64${desktopintegration}.AppImage
 curl --upload-file ./pixsrt-${TRAVIS_TAG}-linux-64${desktopintegration}.AppImage https://transfer.sh/pixsrt-${TRAVIS_TAG}-${CXX}-64${desktopintegration}.AppImage
